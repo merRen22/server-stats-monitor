7d95059f773546e6f6a51af67846caa7cb7035dd
merRen22 <renatomercadoluna@hotmail.com>
2020-03-18 16:03:41 -0500

    first version

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          v.1.0 discord - mc - integrator
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
