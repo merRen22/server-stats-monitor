757c64bca7b14f5fe36e0981fd9fd0bc2f78d145
merRen22 <renatomercadoluna@hotmail.com>
2020-03-20 20:07:01 -0500

    toggle part 1

--- lib/model/themeNotifier.dart
+++ lib/model/themeNotifier.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
 
 final darkTheme = ThemeData(
-  primarySwatch: Colors.grey,
-  primaryColor: Colors.black,
-  brightness: Brightness.dark,
-  backgroundColor: const Color(0xFF212121),
-  accentColor: Colors.white,
-  accentIconTheme: IconThemeData(color: Colors.black),
-  dividerColor: Colors.black12,
-);
+    primarySwatch: Colors.grey,
+    primaryColor: Colors.black,
+    brightness: Brightness.dark,
+    backgroundColor: const Color(0xFF23272A),
+    buttonColor: const Color(0xFF99AAB5),
+    accentColor: Colors.white,
+    accentIconTheme: IconThemeData(color: Colors.black),
+    dividerColor: Colors.black12);
 
 final lightTheme = ThemeData(
   primarySwatch: Colors.grey,
   primaryColor: Colors.white,
   brightness: Brightness.light,
-  backgroundColor: const Color(0xFFE5E5E5),
+  backgroundColor: const Color(0xFF7289DA),
+  buttonColor: const Color(0xFF59F8E8),
+  /*
+  buttonTheme: ButtonThemeData(
+      colorScheme: ColorScheme(
+          primary: null,
+          primaryVariant: null,
+          secondary: null,
+          secondaryVariant: null,
+          surface: null,
+          background: null,
+          error: null,
+          onPrimary: null,
+          onSecondary: null,
+          onSurface: null,
+          onBackground: null,
+          onError: null,
+          brightness: null)),
+          */
   accentColor: Colors.black,
   accentIconTheme: IconThemeData(color: Colors.white),
   dividerColor: Colors.white54,
--- lib/widget/lightManager.dart
+++ lib/widget/lightManager.dart
@@ -4,24 +4,102 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
 import 'package:server_sync/models.dart';
-import 'package:server_sync/styles.dart';
 
 class LightManager extends StatefulWidget {
   @override
   _LightManagerState createState() => _LightManagerState();
 }
 
-class _LightManagerState extends State<LightManager> {
+class FabData {
+  String label;
+  IconData icon;
+  Color color;
+}
+
+class _LightManagerState extends State<LightManager>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 400),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  FabData toggleData(theme) {
+    FabData aux = FabData();
+
+    if (theme) {
+      aux.label = "Light ";
+      aux.icon = FontAwesomeIcons.sun;
+      aux.color = Color(0xFF59F8E8);
+
+      return aux;
+    }
+
+    aux.label = "Dark ";
+    aux.icon = FontAwesomeIcons.moon;
+    aux.color = Color(0xFF03191E);
+
+    return aux;
+  }
+
   @override
   Widget build(BuildContext context) {
     var themeProvider = Provider.of<ThemeNotifier>(context);
 
-    return FloatingActionButton(
-        key: UniqueKey(),
-        backgroundColor:
-            themeProvider.sunny ? kAccentColorLight : Color(0xff2C2F33),
-        child: FaIcon(
-            themeProvider.sunny ? FontAwesomeIcons.sun : FontAwesomeIcons.moon),
-        onPressed: () => themeProvider.toggleSunny());
+    var themeValues = toggleData(themeProvider.sunny);
+
+    return GestureDetector(
+      onTap: () {
+        themeProvider.toggleSunny();
+        controller.reverse().whenComplete(() => controller.forward());
+      },
+      child: Stack(alignment: Alignment.center, children: <Widget>[
+        animatedLabel(themeValues),
+        Positioned(
+            right: 0,
+            child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    border: Border.all(width: 3, color: Colors.white),
+                    color: Color(0xFF23272A),
+                    shape: BoxShape.circle),
+                child: FaIcon(themeValues.icon))),
+      ]),
+    );
+  }
+
+  Widget animatedLabel(themeValues) {
+    final animation =
+        Tween(begin: Offset(60, 0), end: Offset(0, 0)).animate(controller);
+
+    return AnimatedBuilder(
+        animation: animation,
+        child: Container(
+          decoration: BoxDecoration(
+              color: themeValues.color,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  bottomLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                  bottomRight: Radius.circular(20.0))),
+          padding: const EdgeInsets.fromLTRB(8, 5, 48, 5),
+          margin: const EdgeInsets.fromLTRB(8, 10, 0, 10),
+          child: Text(themeValues.label),
+        ),
+        builder: (animatorContext, child) {
+          return Transform.translate(offset: animation.value, child: child);
+        });
   }
 }
